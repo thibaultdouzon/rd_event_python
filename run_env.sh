e44eee7004d47a67aa8da5ac7b71925edf2c4c07
Thibault Douzon <douzont@gmail.com>
2024-09-12 01:27:34 +0200

    adding many files with python code example

--- run_env.sh
+++ run_env.sh
@@ -1,21 +1,2 @@
-# apt update
-# apt upgrade
-
-# VSCODE 
-
-# apt install build-essential
-# apt install ./code.deb
-
-# SSHD
-
-# apt install openssh-server
-# echo 'root:root' | chpasswd
-# sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
-# /usr/sbin/sshd -D &
-
-# 
-
-
 sed -E -i '' '/^\[localhost\]/d' /Users/thibaultdouzon/.ssh/known_hosts
-echo "done"
 docker run -it -p 8000:8000 -p 8022:22 tech_day_python
---
+++ src/main.py
@@ -0,0 +1,66 @@
+import time
+from contextlib import contextmanager
+from typing import Any, Generator, Generic, Optional, TypeVar
+
+from fastapi import FastAPI
+from loguru import logger
+from pydantic import BaseModel
+
+logger.add("app.log", level="INFO")
+
+app = FastAPI()
+
+# Some generic type
+T = TypeVar("T")
+
+
+class User(BaseModel):
+    id: int
+    name: str
+
+
+class Response(BaseModel, Generic[T]):
+    code: int
+    message: str
+    data: Optional[T]
+
+
+database = [
+    User(id=1, name="John Doe"),
+    User(id=2, name="Alice"),
+    User(id=3, name="Bob"),
+]
+
+
+@contextmanager
+def measure_time() -> Generator[None, Any, None]:
+    start_time = time.perf_counter_ns()
+    yield
+    end_time = time.perf_counter_ns()
+    logger.info(f"Execution time: {end_time - start_time:.2f} nano seconds")
+
+
+@app.get("/")
+async def index() -> Response[None]:
+    return Response[None](code=200, message="Hello, World!", data=None)
+
+
+@app.get("/user/{id:int}")
+async def get_user(id: int) -> Response[User]:
+    logger.info(f"Getting user with {id = }")
+    try:
+        with measure_time():
+            index = [user.id for user in database].index(id)
+            user = database[index]
+
+        return Response[User](code=200, message="User found", data=user)
+    except ValueError:
+        logger.error(f"User with {id = } not found")
+        return Response[User](code=404, message="User not found", data=None)
+
+
+@app.post("/user")
+async def create_user(user: User) -> Response[None]:
+    logger.info(f"Appending {user = }")
+    database.append(user)
+    return Response[None](code=201, message="User created", data=None)
